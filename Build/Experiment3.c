e468693ef7077a257021009451744877a51d34e7
Vin1623 <vincenttan1623@gmail.com>
2025-05-14 14:08:55 -0400

    project

--- Build/Experiment3.c
+++ Build/Experiment3.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define N 1000
 
 int main() {
     int n = 1000;
     int matrix[n][n];
+    int i, j;
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            matrix[i][j] = i * n + j + 1;
+    // int *p = (int *) malloc(n*n);
+    // p = &matrix;
+
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            matrix[i][j] = i * n + j + 1; //1 2 3 4 ... 1000, 1001
         }
     }
 
-    for (int i = n - 1; i >= 0; i--) {
-        for (int j = n - 1; j >= 0; j--) {
-            printf("%4d ", matrix[i][j]);
+    for (i = n - 1; i >= 0; i--) {
+        for (j = n - 1; j >= 0; j--) {
+            printf("%6d ", matrix[i][j]);
         }
         printf("\n");
     }
-
+    // free(p);
     return 0;
 }
